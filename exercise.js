b2aa328ae63b123579110dc7510b9f418a6f8e9d
OlamideOladeji <olamideoladeji@gmail.com>
2018-02-21 19:37:39 -0500

    Updated Roman function

--- exercise.js
+++ exercise.js
@@ -2,114 +2,7 @@ var exercise = {};
 
 
 exercise.roman = function(time){
-        var hour = time.getHours();
-        var minutes = time.getMinutes();
-        var seconds = time.getSeconds();
-        if(hour<10){document.getElementById('block5_-1').innerHTML = 'time';
-                    document.getElementById('block6_-1').innerHTML = '';};
 
-        if((hour >10)&&(hour<20)){document.getElementById()};
-
-        switch(hour){
-        case 0:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = '';
-            break;
-        case 1:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 2:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'II';
-            break;
-        case 3:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'III';
-            break;
-        case 4: 
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'IV';
-            break;
-        case 5: 
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'V';
-            break;
-        case 6: 
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'VI';
-            break;
-        case 7:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'VII';
-            break;
-        case 8:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'VIII';
-            break;
-        case 9:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'IX';
-            break;
-        case 10:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 11:
-            document.getElementById('box6_0').innerText() = 'I';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 12:
-            document.getElementById('box6_0').innerText() = 'II';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 13:
-            document.getElementById('box6_0').innerText() = 'III';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 14:
-            document.getElementById('box6_0').innerText() = 'IV';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 15:
-            document.getElementById('box6_0').innerText() = 'V';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 16:
-            document.getElementById('box6_0').innerText() = 'VI';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 17:
-            document.getElementById('box6_0').innerText() = 'VII';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 18:
-            document.getElementById('box6_0').innerText() = 'VII';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 19:
-            document.getElementById('box6_0').innerText() = 'IX';
-            document.getElementById('box5_0').innerText() = 'I';
-            break;
-        case 20:
-            document.getElementById('box6_0').innerText() = '';
-            document.getElementById('box5_0').innerText() = 'II';
-            break;
-        case 21:
-            document.getElementById('box6_0').innerText() = 'I';
-            document.getElementById('box5_0').innerText() = 'II';
-            console.log('confirm'); 
-            break;
-        case 22:
-            document.getElementById('box6_0').innerText() = 'II';
-            document.getElementById('box5_0').innerText() = 'II';
-            break;
-         case 23:
-            document.getElementById('box6_0').innerText() = 'III';
-            document.getElementById('box5_0').innerText() = 'II';
-            break;
-        };
-        //switch(minutes){}
     //-------------------------------------
     //  YOUR CODE
     //    Return an object with roman time. 
@@ -126,216 +19,49 @@ exercise.roman = function(time){
     //     };
     // 
     //-------------------------------------
+    var hour = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        //if(hour<10){document.getElementById('block5_-1').innerHTML = 'time';
+        //           document.getElementById('block6_-1').innerHTML = '';};
 
+        //if((hour >10)&&(hour<20)){document.getElementById()};
+        var hr1stDigit = (Math.floor(hour / 10)) % 10;
+        var hr2ndDigit = hour % 10;
+        var min1stDigit = (Math.floor(minutes / 10)) % 10;
+        var min2ndDigit = minutes % 10;
+        var sec1stDigit = (Math.floor(seconds / 10)) % 10;
+        var sec2ndDigit = seconds % 10;
+        console.log(hour)
+        console.log(minutes)
+        console.log(seconds)
+        document.getElementById('box1_0').textContent = sec2ndDigit;
+        document.getElementById('box2_0').textContent= sec1stDigit;
+        document.getElementById('box3_0').textContent = min2ndDigit;
+        document.getElementById('box4_0').textContent = min1stDigit;
+        document.getElementById('box5_0').textContent = hr2ndDigit;
+        document.getElementById('box6_0').textContent = hr1stDigit;
+
+    
 };
 
-//commas and whatnot below slightly fucked up 
-//missed the col thing 
 exercise.binary = function(time, col){
-    var hour = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
-    switch(hour)
-    {case 0: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 1:
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break; 
-    case 2: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break; 
-    case 3: 
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 4: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break; 
-    case 5: 
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;     
-    case 6: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;   
-    case 7: 
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 8: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "red";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 9: 
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "red";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 10: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "red";
-        break;
-    case 11: 
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 12: 
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 13: 
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 14:
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 15:
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 16:
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 17:
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "red";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 18:
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "red";
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 19:
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "red";  
-        document.getElementbyId('box6_1').style.color = "red";
-        document.getElementbyId('box6_2').style.color = "grey";
-        break;
-    case 20:
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "red";
-        break;
-    case 21:
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "grey";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "red";
-        break;
-    case 22:
-        document.getElementbyId('box5_1').style.color = "grey";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "red";
-        break;
-    case 23:
-        document.getElementbyId('box5_1').style.color = "red";
-        document.getElementbyId('box5_2').style.color = "red";
-        document.getElementbyId('box5_3').style.color = "grey";
-        document.getElementbyId('box5_4').style.color = "grey";
-        document.getElementbyId('box6_1').style.color = "grey";
-        document.getElementbyId('box6_2').style.color = "red";
-        break;    
-    }
-    switch(minutes){
-        case 0:
 
-
-    }
-
-
-    }
+    
+    //----------------------------------------------------------
+    //  YOUR CODE
+    //    Return an object with
+    //    the binary clock values 
+    //    for the given column
+    //
+    //  For example, for time 05:13:47 PM, and column hour_col2
+    //    var binary = { 
+    //        position8 : 'off', 
+    //        position4 : 'on', 
+    //        position2 : 'on', 
+    //        position1 : 'on', 
+    //    }; 
+    // 
+    //----------------------------------------------------------
 
 };
